26e0a113240615cf03b727e02ddaaa151b8f8e12
Justin Rodenbostel <justin@rodenbostel.com>
2015-01-27 09:58:04 -0600

    initial commit

---
+++ app/scripts/controllers.js
@@ -0,0 +1,16 @@
+'use strict';
+angular.module('GettingStartedWithIonicAndNgcordova.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+});
